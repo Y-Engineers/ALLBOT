8d1867eb9991e2d3ba46f7e9cb1ec46c423f6053
DerIch69420 <netroMorten@web.de>
2025-05-25 13:58:10 +0200

    Add pins for receiver

--- receiver/main.py
+++ receiver/main.py
@@ -1,15 +1,27 @@
 # receiver
 
+from machine import Pin, PWM
+
+# communication Pins
+Ptoggle = Pin(1, Pin.OUT) # off->no user control
+PC1 = Pin(2, Pin.OUT) # 1
+PC2 = Pin(3, Pin.OUT) # 2
+PC3 = Pin(4, Pin.OUT) # 4
+PC4 = Pin(5, Pin.OUT) # 8
+Pspeed = PWM(Pin(6), freq=1000)
+Ptimes = PWM(Pin(7), freq=1000)
+
+
 import network
 import espnow
 
 # Set up WLAN in station mode
-w0 = network.WLAN(network.STA_IF)
-w0.active(True)
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
 
-# Initialize ESP-NOW
-e = espnow.ESPNow()
-e.init()
+# init ESP-NOW
+esp = espnow.ESPNow()
+esp.active(True)   # esp.init()
 
 print("Waiting for messages...")
 
--- sender/main.py
+++ sender/main.py
@@ -1,3 +1,5 @@
+# sender
+
 import network
 import espnow
 
@@ -5,7 +7,7 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 
 esp = espnow.ESPNow()
-esp.active(True)   # < esp.init()
+esp.active(True)   # esp.init()
 
 # receiver's MAC
 peer_mac = b'\xc8\xf0\x9eH\xed\xfc'
